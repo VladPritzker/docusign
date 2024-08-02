9c28463eef49ce73fd939ce724b04a54f136df3e
VladPritzker <94125540+VladPritzker@users.noreply.github.com>
2024-08-02 07:20:40 -0400

    fix

---
+++ app/jwt_config.py
@@ -0,0 +1,8 @@
+DS_JWT = {
+    "ds_client_id": "9638a832-3d8d-4c73-b8c4-0323ff3aa1a7",
+    "ds_impersonated_user_id": "10785ab9-dd31-433f-8535-e15e92bb6e3f",  # The id of the user.
+    "private_key_file": "./app/private.key", # Create a new file in your repo source folder named private.key then copy and paste your RSA private key there and save it.
+    "authorization_server": "account-d.docusign.com",
+    "doc_docx": "World_Wide_Corp_Battle_Plan_Trafalgar.docx",
+    "doc_pdf": "World_Wide_Corp_lorem.pdf"
+}
