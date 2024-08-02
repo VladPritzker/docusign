9644c02739186eae344038ae1b04e9c3aac9c5c0
VladPritzker <94125540+VladPritzker@users.noreply.github.com>
2024-08-02 12:23:20 -0400

    fix

--- app/jwt_config.py
+++ app/jwt_config.py
@@ -1,8 +1,12 @@
 DS_JWT = {
     "ds_client_id": "9638a832-3d8d-4c73-b8c4-0323ff3aa1a7",
     "ds_impersonated_user_id": "10785ab9-dd31-433f-8535-e15e92bb6e3f",  # The id of the user.
-    "private_key_file": "./app/private.key", # Create a new file in your repo source folder named private.key then copy and paste your RSA private key there and save it.
+    "private_key_file": "./app/private.key",  # Ensure this file contains your RSA private key.
     "authorization_server": "account-d.docusign.com",
+    "api_account_id": "f264ffa6-297c-44af-be90-cfa403ffd84b",
+    "base_uri": "https://demo.docusign.net",
     "doc_docx": "World_Wide_Corp_Battle_Plan_Trafalgar.docx",
-    "doc_pdf": "World_Wide_Corp_lorem.pdf"
+    "doc_pdf": "World_Wide_Corp_lorem.pdf",
+    "email":"pritzkervlad@gmail.com"
+
 }
--- jwt_get_envelop_info.py
+++ jwt_get_envelop_info.py
@@ -1,66 +1,63 @@
 import sys
 import subprocess
+from os import path
 
-from docusign_esign import ApiClient, EnvelopesApi
+from docusign_esign import ApiClient
 from docusign_esign.client.api_exception import ApiException
+from docusign_esign.apis import EnvelopesApi  # Import EnvelopesApi correctly
 from app.jwt_helpers import get_jwt_token, get_private_key
 from app.jwt_config import DS_JWT
 
 # pip install DocuSign SDK
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'docusign_esign'])
 
-SCOPES = ["signature", "impersonation"]
+SCOPES = [
+    "signature", "impersonation"
+]
 
 def get_consent_url():
     url_scopes = "+".join(SCOPES)
     redirect_uri = "https://developers.docusign.com/platform/auth/consent"
-    consent_url = f"https://{DS_JWT['authorization_server']}/oauth/auth?response_type=code&scope={url_scopes}&client_id={DS_JWT['ds_client_id']}&redirect_uri={redirect_uri}"
+    consent_url = f"https://{DS_JWT['authorization_server']}/oauth/auth?response_type=code&" \
+                  f"scope={url_scopes}&client_id={DS_JWT['ds_client_id']}&redirect_uri={redirect_uri}"
     return consent_url
 
 def get_token(private_key, api_client):
-    token_response = get_jwt_token(private_key, SCOPES, DS_JWT["authorization_server"], DS_JWT["ds_client_id"], DS_JWT["ds_impersonated_user_id"])
+    token_response = get_jwt_token(private_key, SCOPES, DS_JWT["authorization_server"], DS_JWT["ds_client_id"],
+                                   DS_JWT["ds_impersonated_user_id"])
     access_token = token_response.access_token
-
     user_info = api_client.get_user_info(access_token)
     accounts = user_info.get_accounts()
     api_account_id = accounts[0].account_id
     base_path = accounts[0].base_uri + "/restapi"
-
     return {"access_token": access_token, "api_account_id": api_account_id, "base_path": base_path}
 
 def get_args(api_account_id, access_token, base_path):
-    signer_email = input("Please enter the signer's email address: ")
-    signer_name = input("Please enter the signer's name: ")
-    cc_email = input("Please enter the cc email address: ")
-    cc_name = input("Please enter the cc name: ")
+    envelope_id = input("Please enter the envelope ID: ")
 
-    envelope_args = {
-        "signer_email": signer_email,
-        "signer_name": signer_name,
-        "cc_email": cc_email,
-        "cc_name": cc_name,
-        "status": "sent",
-    }
     args = {
         "account_id": api_account_id,
         "base_path": base_path,
         "access_token": access_token,
-        "envelope_args": envelope_args
+        "envelope_id": envelope_id
     }
 
     return args
 
 def fetch_envelope_data(api_client, args):
-    envelope_id = input("Please enter the envelope ID: ")
+    envelopes_api = EnvelopesApi(api_client)  # Correctly instantiate EnvelopesApi
     try:
-        envelopes_api = EnvelopesApi(api_client)
-        envelope_data = envelopes_api.get_envelope(args["account_id"], envelope_id)
-        print(f"Envelope data: {envelope_data}")
+        envelope = envelopes_api.get_envelope(args["account_id"], args["envelope_id"])
+        envelope_documents = envelopes_api.list_documents(args["account_id"], args["envelope_id"])
+        print("Envelope status:", envelope.status)
+        for document in envelope_documents.envelope_documents:
+            print("Document ID:", document.document_id)
+            print("Document Name:", document.name)
     except ApiException as e:
-        print(f"Error while retrieving envelope data: {e}")
-        print(f"Reason: {e.reason}")
-        print(f"HTTP response headers: {e.headers}")
-        print(f"HTTP response body: {e.body}")
+        print("Error while retrieving envelope data:", e)
+        print("Reason:", e.reason)
+        print("HTTP response headers:", e.headers)
+        print("HTTP response body:", e.body.decode('utf8'))
 
 def run_example(private_key, api_client):
     jwt_values = get_token(private_key, api_client)
@@ -69,7 +66,7 @@ def run_example(private_key, api_client):
 
 def main():
     api_client = ApiClient()
-    api_client.set_base_path(DS_JWT["authorization_server"])
+    api_client.set_base_path(DS_JWT["base_uri"])
     api_client.set_oauth_host_name(DS_JWT["authorization_server"])
 
     private_key = get_private_key(DS_JWT["private_key_file"]).encode("ascii").decode("utf-8")
@@ -78,7 +75,6 @@ def main():
         run_example(private_key, api_client)
     except ApiException as err:
         body = err.body.decode('utf8')
-
         if "consent_required" in body:
             consent_url = get_consent_url()
             print("Open the following URL in your browser to grant consent to the application:")
@@ -90,3 +86,6 @@ def main():
                 sys.exit("Please grant consent")
 
 main()
+
+
+# 61c54ffd-703f-4b49-9cf8-3375a5ab4c59
